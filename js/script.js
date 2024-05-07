b7e15f23fdbb172c344d30499fac8bad90bee6d9
Daria <dashapetrash@gmail.com>
2024-05-07 09:56:14 +0200

    add objects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-let ldl = 1;
+let numbersOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?');
 
-const obj = {
-    a: 10
+
+let personalMovieDB = {
+    'count': numbersOfFilms,
+    'movies': {},
+    'actors': {},
+    'genders': [],
+    'privat': false
 };
-obj.a = 6;
 
-console.log(obj);
+const a = prompt('Один из последних просмотренных фильмов?'),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов?'),
+      d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;  
+personalMovieDB.movies[c] = d;  
+console.log(personalMovieDB);
